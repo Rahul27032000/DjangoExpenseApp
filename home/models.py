70450ace7516a04d82506de77fd14669d2e4755c
Shinde Rahul <shinde2732000@gmail.com>
2023-08-29 10:59:37 +0530

    added profile functionlaity to project

--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+TYPE =(
+    ('Positive','Positive'),
+    ('Positive','Positive')
+    )
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    income = models.FloatField()
+    balance = models.FloatField(null=True)
+
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    expense_type = models.CharField(max_length=100, choices=TYPE)
+
+
+    def __str__(self):
+        return self.name
+    
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    user = request.user
+    profile = Profile.objects.filter(user=user).first()
+    print(user)
+    context={'profile': profile}
+    return render(request, 'home/home.html',context)
